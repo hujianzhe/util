0b5f33b890d9b3b6647e512e988bd938ceb75f69
hujianzhe <976784480@qq.com>
2017-12-11 16:27:23 +0800

    compiler_define

--- c/compiler_define.h
+++ c/compiler_define.h
@@ -5,35 +5,28 @@
 #ifndef UTIL_C_COMPILER_DEFINE_H
 #define	UTIL_C_COMPILER_DEFINE_H
 
-#define	COMPILE_CHECK(exp)	typedef char __compile_check__[(exp) ? 1 : -1]
+#define	compiler_check(exp)						typedef char __compile_check__[(exp) ? 1 : -1]
 
 #define	field_sizeof(type, field)				sizeof(((type*)0)->field)
 #define	field_offset(type, field)				((char*)(&((type *)0)->field) - (char*)(0))
 #define field_container(address, type, field)	((type *)((char*)(address) - (char*)(&((type *)0)->field)))
 
-#if defined(_WIN32) || defined(_WIN64)
-	#ifdef _MSC_VER
-		#ifdef	_WIN64
-			/* long long*/
-			typedef	__int64		ssize_t;
-		#else
-			/* long */
-			typedef	int			ssize_t;
-		#endif
-		#pragma warning(disable:4200)
-		#pragma warning(disable:4018)
-		#pragma warning(disable:4244)
-		#pragma warning(disable:4267)
-		#pragma warning(disable:4800)
-		#pragma warning(disable:4819)
-		#ifndef _CRT_SECURE_NO_WARNINGS
-			#define	_CRT_SECURE_NO_WARNINGS
-		#endif
-		#ifndef _WINSOCK_DEPRECATED_NO_WARNINGS
-			#define	_WINSOCK_DEPRECATED_NO_WARNINGS
-		#endif
+#ifdef _MSC_VER
+	#pragma warning(disable:4200)
+	#pragma warning(disable:4018)
+	#pragma warning(disable:4244)
+	#pragma warning(disable:4267)
+	#pragma warning(disable:4800)
+	#pragma warning(disable:4819)
+	#ifndef _CRT_SECURE_NO_WARNINGS
+		#define	_CRT_SECURE_NO_WARNINGS
 	#endif
-#else
+	#ifndef _WINSOCK_DEPRECATED_NO_WARNINGS
+		#define	_WINSOCK_DEPRECATED_NO_WARNINGS
+	#endif
+	#define	variable_align(alignment)			__declspec(align(alignment))
+
+#elif	defined(__GNUC__) || defined(__GNUG__)
 	#ifndef	_XOPEN_SOURCE
 		#define	_XOPEN_SOURCE
 	#endif
@@ -42,6 +35,10 @@
 			#define _GNU_SOURCE
 		#endif
 	#endif
+	#define	variable_align(alignment)			__attribute__ ((aligned(alignment)))
+
+#else
+	#error "Unknown Compiler"
 #endif
 
 #endif
