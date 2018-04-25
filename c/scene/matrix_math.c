d6a78647e857c35f8bd1d3225cc7fbcda3e96e81
hujianzhe <976784480@qq.com>
2018-04-25 15:41:31 +0800

    matrix

--- c/scene/matrix_math.c
+++ c/scene/matrix_math.c
@@ -10,14 +10,58 @@
 #endif
 #endif
 #include <math.h>
+#include <stdlib.h>
 
 #ifdef	__cplusplus
 extern "C" {
 #endif
 
+void matrix_remove_row(struct matrix_t* m, unsigned int row) {
+	unsigned int i, j;
+
+	if (m->row <= row)
+		return;
+
+	for (i = row; i < m->row && i + 1 < m->row; ++i) {
+		for (j = 0; j < m->col; ++j) {
+			m->val[i * m->col + j] = m->val[(i + 1) * m->col + j];
+		}
+	}
+	m->row -= 1;
+}
+
+void matrix_remove_col(struct matrix_t* m, unsigned int col) {
+	unsigned int i;
+
+	if (m->col <= col)
+		return;
+
+	for (i = col; i < m->col && i + 1 < m->col; ++i) {
+		m->val[i] = m->val[i + 1];
+	}
+	m->col -= 1;
+}
+
 double matrix_det(const struct matrix_t* m) {
-	//return __matrix_det(m, 0, 0, 0);
-	return 0.0;
+	if (m->row != m->col || !m->row)
+		return NAN;
+	switch (m->row) {
+		case 1:
+			return	matrix_val(m,0,0);
+		case 2:
+			return	matrix_val(m,0,0)*matrix_val(m,1,1) - matrix_val(m,0,1)*matrix_val(m,1,0);
+		case 3:
+			return	matrix_val(m,0,0)*matrix_val(m,1,1)*matrix_val(m,2,2) -
+					matrix_val(m,0,0)*matrix_val(m,1,2)*matrix_val(m,2,1) -
+					matrix_val(m,0,1)*matrix_val(m,1,0)*matrix_val(m,2,2) +
+					matrix_val(m,0,1)*matrix_val(m,1,2)*matrix_val(m,2,0) +
+					matrix_val(m,0,2)*matrix_val(m,1,0)*matrix_val(m,2,1) -
+					matrix_val(m,0,2)*matrix_val(m,1,1)*matrix_val(m,2,0);
+		default:
+		{
+			return 0.0;
+		}
+	}
 }
 
 #ifdef	__cplusplus
--- c/scene/matrix_math.h
+++ c/scene/matrix_math.h
@@ -7,13 +7,17 @@
 
 typedef struct matrix_t {
 	unsigned int row, col;
-	double** val;
+	double* val;
 } matrix_t;
+
+#define	matrix_val(m, r, c)	((m)->val[(r) * (m)->col + (c)])
 
 #ifdef	__cplusplus
 extern "C" {
 #endif
 
+void matrix_remove_row(struct matrix_t* m, unsigned int row);
+void matrix_remove_col(struct matrix_t* m, unsigned int col);
 double matrix_det(const struct matrix_t* m);
 
 #ifdef	__cplusplus
