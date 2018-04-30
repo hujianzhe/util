42c227e58abde15f0e271ed37073299178ccf6ed
hujianzhe <976784480@qq.com>
2018-04-30 16:48:32 +0800

    shape3d

---
+++ c/scene/shape3d.c
@@ -0,0 +1,52 @@
+//
+// Created by hujianzhe on 18-4-30.
+//
+
+#include "shape3d.h"
+#include <errno.h>
+#include <float.h>
+#if defined(_WIN32) || defined(_WIN64)
+	#ifndef	_USE_MATH_DEFINES
+		#define	_USE_MATH_DEFINES
+	#endif
+#endif
+#include <math.h>
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+static int shape3d_linesegment_has_point(const vector3_t* s, const vector3_t* e, const vector3_t* p) {
+	return 0;
+}
+
+static unsigned int shape3d_linesegment_has_point_n(const shape3d_linesegment_t* ls, const vector3_t point[], unsigned int n) {
+	unsigned int i;
+	for (i = 0; i < n; ++i) {
+		if (!shape3d_linesegment_has_point(&ls->vertices0, &ls->vertices1, point + i))
+			break;
+	}
+	return i;
+}
+
+static int shape3d_aabb_has_point(const shape3d_aabb_t* aabb, const vector3_t* point) {
+	return aabb->pivot.x - aabb->half.x <= point->x
+		&& aabb->pivot.x + aabb->half.x >= point->x
+		&& aabb->pivot.y - aabb->half.y <= point->y
+		&& aabb->pivot.y + aabb->half.y >= point->y
+		&& aabb->pivot.z - aabb->half.z <= point->z
+		&& aabb->pivot.z + aabb->half.z >= point->z;
+}
+
+static unsigned int shape3d_aabb_has_point_n(const shape3d_aabb_t* aabb, const vector3_t point[], unsigned int n) {
+	unsigned int i;
+	for (i = 0; i < n; ++i) {
+		if (!shape3d_aabb_has_point(aabb, point + i))
+			break;
+	}
+	return i;
+}
+
+#ifdef	__cplusplus
+}
+#endif
---
+++ c/scene/shape3d.h
@@ -0,0 +1,37 @@
+//
+// Created by hujianzhe on 18-4-30.
+//
+
+#ifndef UTIL_C_SCENE_SHAPE3D_H
+#define	UTIL_C_SCENE_SHAPE3D_H
+
+#include "vector_math.h"
+
+typedef enum shape3d_enum_t {
+	SHAPE3D_MIN_ENUM_NUM = -1,
+	SHAPE3D_LINESEGMENT,
+	SHAPE3D_AABB,
+	SHAPE3D_MAX_ENUM_NUM
+} shape3d_enum_t;
+
+typedef struct shape3d_linesegment_t {
+	vector3_t vertices0;
+	vector3_t vertices1;
+} shape3d_linesegment_t;
+
+typedef struct shape3d_aabb_t {
+	vector3_t pivot;
+	vector3_t half;
+} shape3d_aabb_t;
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif // !UTIL_C_SCENE_SHAPE3D_H
