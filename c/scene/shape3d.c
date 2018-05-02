6a511b04f4078fdf9366d8e669be427b366d0327
hujianzhe <976784480@qq.com>
2018-05-02 18:19:35 +0800

    shape3d

--- c/scene/shape3d.c
+++ c/scene/shape3d.c
@@ -54,6 +54,53 @@ static unsigned int shape3d_aabb_has_point_n(const shape3d_aabb_t* aabb, const v
 	return i;
 }
 
+static int shape3d_sphere_has_point(const shape3d_sphere_t* sphere, const vector3_t* point) {
+	vector3_t v = { point->x - sphere->pivot.x, point->y - sphere->pivot.y, point->z - sphere->pivot.z };
+	return vector3_lensq(&v) <= sphere->radius * sphere->radius;
+}
+
+static unsigned int shape3d_sphere_has_point_n(const shape3d_sphere_t* sphere, const vector3_t point[], unsigned int n) {
+	unsigned int i;
+	for (i = 0; i < n; ++i) {
+		if (!shape3d_sphere_has_point(sphere, point + i))
+			break;
+	}
+	return i;
+}
+
+static int shape3d_cylinder_has_point(const shape3d_cylinder_t* cylinder, const vector3_t* point) {
+	double d;
+	vector3_t v = { point->x - cylinder->pivot0.x, point->y - cylinder->pivot0.y, point->z - cylinder->pivot0.z };
+	vector3_t p = { cylinder->pivot1.x - cylinder->pivot0.x, cylinder->pivot1.y - cylinder->pivot0.y, cylinder->pivot1.z - cylinder->pivot0.z };
+	vector3_normalized(&p, &p);
+	d = vector3_dot(&p, &v);
+	p.x = p.x * d + cylinder->pivot0.x;
+	p.y = p.y * d + cylinder->pivot0.y;
+	p.z = p.z * d + cylinder->pivot0.z;
+	if (point->x >= fmin(cylinder->pivot0.x, cylinder->pivot1.x) &&
+		point->x <= fmax(cylinder->pivot0.x, cylinder->pivot1.x) &&
+		point->y >= fmin(cylinder->pivot0.y, cylinder->pivot1.y) &&
+		point->y <= fmax(cylinder->pivot0.y, cylinder->pivot1.y) &&
+		point->z >= fmin(cylinder->pivot0.z, cylinder->pivot1.z) &&
+		point->z <= fmax(cylinder->pivot0.z, cylinder->pivot1.z))
+	{
+		v.x = point->x - p.x;
+		v.y = point->y - p.y;
+		v.z = point->z - p.z;
+		return vector3_lensq(&v) <= cylinder->radius * cylinder->radius;
+	}
+	return 0;
+}
+
+static unsigned int shape3d_cylinder_has_point_n(const shape3d_cylinder_t* cylinder, const vector3_t point[], unsigned int n) {
+	unsigned int i;
+	for (i = 0; i < n; ++i) {
+		if (!shape3d_cylinder_has_point(cylinder, point + i))
+			break;
+	}
+	return i;
+}
+
 #ifdef	__cplusplus
 }
 #endif
--- c/scene/shape3d.h
+++ c/scene/shape3d.h
@@ -11,6 +11,8 @@ typedef enum shape3d_enum_t {
 	SHAPE3D_MIN_ENUM_NUM = -1,
 	SHAPE3D_LINESEGMENT,
 	SHAPE3D_AABB,
+	SHAPE3D_SPHERE,
+	SHAPE3D_CAPSULE,
 	SHAPE3D_MAX_ENUM_NUM
 } shape3d_enum_t;
 
@@ -23,6 +25,17 @@ typedef struct shape3d_aabb_t {
 	vector3_t pivot;
 	vector3_t half;
 } shape3d_aabb_t;
+
+typedef struct shape3d_sphere_t {
+	vector3_t pivot;
+	double radius;
+} shape3d_sphere_t;
+
+typedef struct shape3d_cylinder_t {
+	vector3_t pivot0;
+	vector3_t pivot1;
+	double radius;
+} shape3d_cylinder_t;
 
 #ifdef	__cplusplus
 extern "C" {
