85f155d66bcd846ab7dd1d33548f957a106a7b68
hujianzhe <976784480@qq.com>
2018-05-08 16:39:48 +0800

    shape3d

--- c/scene/shape3d.c
+++ c/scene/shape3d.c
@@ -36,10 +36,12 @@ static unsigned int shape3d_linesegment_has_point_n(const shape3d_linesegment_t*
 	return i;
 }
 
-static shape3d_plane_t* shape3d_build_plane_by_point(shape3d_plane_t* plane, const vector3_t* p1, const vector3_t* p2, const vector3_t* p3) {
+static shape3d_plane_t* shape3d_plane_build_by_point(shape3d_plane_t* plane, const vector3_t* p1, const vector3_t* p2, const vector3_t* p3) {
 	vector3_t p12 = { p2->x - p1->x, p2->y - p1->y, p2->z - p1->z };
 	vector3_t p13 = { p3->x - p1->x, p3->y - p1->y, p3->z - p1->z };
 	vector3_cross(&plane->normal, &p12, &p13);
+	if (vector3_is_zero(&plane->normal))
+		return NULL;
 	vector3_normalized(&plane->normal, &plane->normal);
 	plane->pivot = *p1;
 	return plane;
@@ -58,6 +60,83 @@ static unsigned int shape3d_plane_has_point_n(const shape3d_plane_t* plane, cons
 			break;
 	}
 	return i;
+}
+
+static int shape3d_linesegment_has_intersect(const vector3_t* s1, const vector3_t* e1, const vector3_t* s2, const vector3_t* e2) {
+	int l1 = 0, l2 = 0;
+	double min_x, min_y, min_z, max_x, max_y, max_z, d;
+	vector3_t res1, res2;
+	vector3_t s1e1 = { e1->x - s1->x, e1->y - s1->y, e1->z - s1->z };
+	vector3_t s1s2 = { s2->x - s1->x, s2->y - s1->y, s2->z - s1->z };
+	vector3_t s1e2 = { e2->x - s1->x, e2->y - s1->y, e2->z - s1->z };
+	vector3_t s2e2 = { e2->x - s2->x, e2->y - s2->y, e2->z - s2->z };
+	vector3_t s2s1 = { s1->x - s2->x, s1->y - s2->y, s1->z - s2->y };
+	vector3_t s2e1 = { e1->x - s2->x, e1->y - s2->y, e1->z - s2->z };
+	
+	min_x = fmin(s1->x, e1->x);
+	min_y = fmin(s1->y, e1->y);
+	min_z = fmin(s1->z, e1->z);
+	max_x = fmax(s1->x, e1->x);
+	max_y = fmax(s1->y, e1->y);
+	max_z = fmax(s1->z, e1->z);
+	if (vector3_is_zero(vector3_cross(&res1, &s1e1, &s1s2))) {
+		if (s2->x >= min_x && s2->x <= max_x &&
+			s2->y >= min_y && s2->y <= max_y &&
+			s2->z >= min_z && s2->z <= max_z)
+		{
+			return 1;
+		}
+		l1 = 1;
+	}
+	if (vector3_is_zero(vector3_cross(&res1, &s1e1, &s1e2))) {
+		if (e2->x >= min_x && e2->x <= max_x &&
+			e2->y >= min_y && e2->y <= max_y &&
+			e2->z >= min_z && e2->z <= max_z)
+		{
+			return 1;
+		}
+		l2 = 1;
+	}
+
+	min_x = fmin(s2->x, e2->x);
+	min_y = fmin(s2->y, e2->y);
+	min_z = fmin(s2->z, e2->z);
+	max_x = fmax(s2->x, e2->x);
+	max_y = fmax(s2->y, e2->y);
+	max_z = fmax(s2->z, e2->z);
+	if (vector3_is_zero(vector3_cross(&res1, &s2e2, &s2s1)) &&
+		s1->x >= min_x && s1->x <= max_x &&
+		s1->y >= min_y && s1->y <= max_y &&
+		s1->z >= min_z && s1->z <= max_z)
+	{
+		return 1;
+	}
+	if (vector3_is_zero(vector3_cross(&res1, &s2e2, &s2e1)) &&
+		e1->x >= min_x && e1->x <= max_x &&
+		e1->y >= min_y && e1->y <= max_y &&
+		e1->z >= min_z && e1->z <= max_z)
+	{
+		return 1;
+	}
+
+	if (l1 && l2) {
+		return 0;
+	}
+	else if (l1) {
+		vector3_cross(&res1, &s1e1, &s1e2);
+		vector3_normalized(&res1, &res1);
+		d = vector3_dot(&res1, &s1s2);
+	}
+	else {
+		vector3_cross(&res1, &s1e1, &s1s2);
+		vector3_normalized(&res1, &res1);
+		d = vector3_dot(&res1, &s1e2);
+	}
+	if (d <= -DBL_EPSILON || d >= DBL_EPSILON) {
+		return 0;
+	}
+	return vector3_dot(vector3_cross(&res1, &s1s2, &s1e1), vector3_cross(&res2, &s1e2, &s1e1)) < 0
+		&& vector3_dot(vector3_cross(&res1, &s2s1, &s2e2), vector3_cross(&res2, &s2e1, &s2e2)) < 0;
 }
 
 static unsigned int shape3d_plane_has_contain_linesegment(const shape3d_plane_t* plane, const shape3d_linesegment_t* ls) {
