664953621b82a2d33dd85fdac752703a21620929
hujianzhe <976784480@qq.com>
2018-07-23 00:30:40 +0800

    ............

--- cpp/protocol/packet_worker.h
+++ cpp/protocol/packet_worker.h
@@ -5,22 +5,15 @@
 #ifndef UTIL_CPP_PROTOCOL_PACKET_WORKER_H
 #define	UTIL_CPP_PROTOCOL_PACKET_WORKER_H
 
-#include "../../c/syslib/socket.h"
+struct sockaddr_storage;
 
 namespace Util {
 class PacketWorker {
 public:
-	PacketWorker(void) : m_userdata(NULL) {}
 	virtual ~PacketWorker(void) {}
-
-	void userdata(void* data) { m_userdata = data; }
-	void* userdata(void) const { return m_userdata; }
 
 	virtual int onParsePacket(unsigned char* buf, size_t buflen, struct sockaddr_storage* from) = 0;
 	virtual bool onRecvPacket(unsigned char* data, size_t len, struct sockaddr_storage* from) { return true; }
-
-private:
-	void* m_userdata;
 };
 }
 
