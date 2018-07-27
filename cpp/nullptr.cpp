97e1dc3819e80d7929c07ab8598fa0784bdef750
hujianzhe <976784480@qq.com>
2018-07-27 13:21:20 +0800

    nullptr

---
+++ cpp/nullptr.cpp
@@ -0,0 +1,9 @@
+//
+// Created by hujianzhe on 18-7-27
+//
+
+#include "nullptr.h"
+
+#if	__CPP_VERSION < 2011
+std::nullptr_t nullptr;
+#endif
---
+++ cpp/nullptr.h
@@ -0,0 +1,22 @@
+//
+// Created by hujianzhe on 18-7-27
+//
+
+#ifndef	UTIL_CPP_NULLPTR_H
+#define	UTIL_CPP_NULLPTR_H
+
+#include "cpp_compiler_define.h"
+#include <cstddef>
+#if	__CPP_VERSION < 2011
+namespace std {
+typedef struct nullptr_t {
+	template <typename T>
+		operator T*() { return (T*)0; }
+private:
+	void* __p;
+} nullptr_t;
+}
+extern std::nullptr_t nullptr;
+#endif
+
+#endif
