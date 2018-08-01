2301fd1b359c22d2b3069aca893ea916b529eec4
hujianzhe <976784480@qq.com>
2018-08-01 15:37:29 +0800

    fixed bug

--- cpp/exception.h
+++ cpp/exception.h
@@ -6,21 +6,24 @@
 #define UTIL_CPP_EXCEPTION_H
 
 #include <stdio.h>
+#include <string.h>
 #include <exception>
 #include <stdexcept>
 
 #define	assert_throw(expression) {\
 if (!(expression)) {\
+const char* _expression_str = #expression;\
 char _str[512];\
-snprintf(_str, sizeof(_str), "%s(%d): " #expression "\n", __FILE__, __LINE__); \
+snprintf(_str, sizeof(_str), "%s(%d): %s\r\n", __FILE__, __LINE__, _expression_str); \
 throw std::logic_error(_str);\
 }\
 }
 
 #define	logic_throw(expression, fmt, ...) {\
 if (!(expression)) {\
+const char* _expression_str = #expression;\
 char _str[512];\
-snprintf(_str, sizeof(_str), "%s(%d): " #expression "\n" fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);\
+snprintf(_str, sizeof(_str), "%s(%d): %s\r\n" fmt "\r\n", __FILE__, __LINE__, _expression_str, ##__VA_ARGS__);\
 throw std::logic_error(_str);\
 }\
 }
