c96508dc1817d3bcec3612f2f86d7cf7e742c1d3
hujianzhe <976784480@qq.com>
2018-08-25 21:02:32 +0800

    unique_ptr

--- cpp/unique_ptr.h
+++ cpp/unique_ptr.h
@@ -107,6 +107,9 @@ template <class T1, class D, class T2>
 bool operator==(const T2* y, const unique_ptr<T1, D>& x) { return x.get() == y; }
 template <class T, class D>
 bool operator==(nullptr_t p, const unique_ptr<T, D>& x) { return x.get() == p; }
+template <class T, class D>
+bool operator==(const unique_ptr<T, D>& x, nullptr_t p, ) { return x.get() == p; }
+
 template <class T1, class D1, class T2, class D2>
 bool operator!=(const unique_ptr<T1, D1>& x, const unique_ptr<T2, D2>& y) { return x.get() != y.get(); }
 template <class T1, class D, class T2>
@@ -115,6 +118,8 @@ template <class T1, class D, class T2>
 bool operator!=(const T2* y, const unique_ptr<T1, D>& x) { return x.get() != y; }
 template <class T, class D>
 bool operator!=(nullptr_t p, const unique_ptr<T, D>& x) { return x.get() != p; }
+template <class T, class D>
+bool operator!=(const unique_ptr<T, D>& x, nullptr_t p) { return x.get() != p; }
 }
 #endif
 
