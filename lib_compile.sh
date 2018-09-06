7fcdd68384b6e5a9db21d5a8e7539ae3fa2541f3
hujianzhe <976784480@qq.com>
2018-09-06 11:11:57 +0800

    ..............

--- lib_compile.sh
+++ lib_compile.sh
@@ -10,17 +10,13 @@ fi
 SOURCE_FILE=$SOURCE_C_FILE" $SOURCE_CPP_FILE"
 MACRO="-D_REENTRANT"
 COMPILE_OPTION="-Wno-deprecated -Wno-parentheses"
-DEFAULT_LINK="-lpthread -lm -ldl"
 TARGET="libutil.a"
-if [ `uname` = "Linux" ];then
-	DEFAULT_LINK=$DEFAULT_LINK" -lrt -lcrypto -luuid"
-fi
 
 rm $OBJECT_FILE 2>/dev/null
 rm $TARGET 2>/dev/null
 find ./ -type f -exec touch {} \;
 echo $COMPILER is compiling......
-$COMPILER -c $MACRO $COMPILE_OPTION $SOURCE_FILE $DEFAULT_LINK
+$COMPILER -c $MACRO $COMPILE_OPTION $SOURCE_FILE
 if [ "$?" != 0 ];then
 	OBJECT_FILE=`find . -name "*.o"`
 	rm $OBJECT_FILE 2>/dev/null
