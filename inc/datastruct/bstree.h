5cebe3d9f631744b4b38d678b76318a4c34998df
hujianzhe <976784480@qq.com>
2019-08-19 02:52:08 +0800

    binary search tree

---
+++ inc/datastruct/bstree.h
@@ -0,0 +1,40 @@
+//
+// Created by hujianzhe
+//
+
+#ifndef	UTIL_C_DATASTRUCT_BSTREE_H
+#define	UTIL_C_DATASTRUCT_BSTREE_H
+
+#include "../compiler_define.h"
+
+struct BSTree_t;
+typedef struct BSTreeNode_t {
+	const void* key;
+	struct BSTreeNode_t* bs_left;
+	struct BSTreeNode_t* bs_right;
+	struct BSTreeNode_t* bs_parent;
+	struct BSTree_t* bs_tree;
+} BSTreeNode_t;
+
+typedef struct BSTree_t {
+	struct BSTreeNode_t* bs_root;
+	int (*keycmp)(const void*, const void*);
+} BSTree_t;
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+__declspec_dll struct BSTree_t* bstreeInit(struct BSTree_t* tree, int(*keycmp)(const void*, const void*));
+__declspec_dll struct BSTreeNode_t* bstreeInsertNode(struct BSTree_t* tree, struct BSTreeNode_t* node);
+__declspec_dll void bstreeReplaceNode(struct BSTreeNode_t* old_node, struct BSTreeNode_t* new_node);
+__declspec_dll void bstreeRemoveNode(struct BSTree_t* tree, struct BSTreeNode_t* node);
+__declspec_dll struct BSTreeNode_t* bstreeMinNode(const struct BSTreeNode_t* node);
+__declspec_dll struct BSTreeNode_t* bstreeMaxNode(const struct BSTreeNode_t* node);
+__declspec_dll struct BSTreeNode_t* bstreeSearchKey(struct BSTree_t* tree, const void* key);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif
---
+++ src/datastruct/bstree.c
@@ -0,0 +1,143 @@
+//
+// Created by hujianzhe
+//
+
+#include "../../inc/datastruct/bstree.h"
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+struct BSTree_t* bstreeInit(struct BSTree_t* tree, int(*keycmp)(const void*, const void*)) {
+	tree->bs_root = (struct BSTreeNode_t*)0;
+	tree->keycmp = keycmp;
+	return tree;
+}
+
+struct BSTreeNode_t* bstreeInsertNode(struct BSTree_t* tree, struct BSTreeNode_t* node) {
+	struct BSTreeNode_t* parent = tree->bs_root;
+	while (parent) {
+		int res = tree->keycmp(parent->key, node->key);
+		if (res < 0) {
+			if (parent->bs_left)
+				parent = parent->bs_left;
+			else {
+				parent->bs_left = node;
+				break;
+			}
+		}
+		else if (res > 0) {
+			if (parent->bs_right)
+				parent = parent->bs_right;
+			else {
+				parent->bs_right = node;
+				break;
+			}
+		}
+		else
+			return parent;
+	}
+	if (!parent)
+		tree->bs_root = node;
+	node->bs_parent = parent;
+	node->bs_left = node->bs_right = (struct BSTreeNode_t*)0;
+	node->bs_tree = tree;
+	return node;
+}
+
+void bstreeReplaceNode(struct BSTreeNode_t* old_node, struct BSTreeNode_t* new_node) {
+	if (old_node && old_node != new_node) {
+		const void* key = new_node->key;
+		if (old_node->bs_left)
+			old_node->bs_left->bs_parent = new_node;
+		if (old_node->bs_right)
+			old_node->bs_right->bs_parent = new_node;
+		if (old_node->bs_parent) {
+			if (old_node->bs_parent->bs_left == old_node)
+				old_node->bs_parent->bs_left = new_node;
+			if (old_node->bs_parent->bs_right == old_node)
+				old_node->bs_parent->bs_right = new_node;
+		}
+		else {
+			old_node->bs_tree->bs_root = new_node;
+		}
+		*new_node = *old_node;
+		new_node->key = key;
+	}
+}
+
+void bstreeRemoveNode(struct BSTree_t* tree, struct BSTreeNode_t* node) {
+	if (node->bs_right) {
+		struct BSTreeNode_t* min_node = node->bs_right;
+		while (min_node->bs_left)
+			min_node = min_node->bs_left;
+		if (node->bs_parent) {
+			if (node->bs_parent->bs_left == node)
+				node->bs_parent->bs_left = min_node;
+			else
+				node->bs_parent->bs_right = min_node;
+		}
+		else
+			tree->bs_root = min_node;
+		if (node->bs_left)
+			node->bs_left->bs_parent = min_node;
+		min_node->bs_left = node->bs_left;
+		if (min_node->bs_parent != node) {
+			node->bs_right->bs_parent = min_node;
+			min_node->bs_parent->bs_left = min_node->bs_right;
+			if (min_node->bs_right)
+				min_node->bs_right->bs_parent = min_node->bs_parent;
+			min_node->bs_right = node->bs_right;
+		}
+		min_node->bs_parent = node->bs_parent;
+	}
+	else if (node->bs_left) {
+		node->bs_left->bs_parent = node->bs_parent;
+		if (node->bs_parent) {
+			if (node->bs_parent->bs_left == node)
+				node->bs_parent->bs_left = node->bs_left;
+			else
+				node->bs_parent->bs_right = node->bs_left;
+		}
+		else
+			node->bs_tree->bs_root = node->bs_left;
+	}
+	else if (node->bs_parent) {
+		if (node->bs_parent->bs_left == node)
+			node->bs_parent->bs_left = (struct BSTreeNode_t*)0;
+		else
+			node->bs_parent->bs_right = (struct BSTreeNode_t*)0;
+	}
+	else
+		tree->bs_root = (struct BSTreeNode_t*)0;
+}
+
+struct BSTreeNode_t* bstreeMinNode(const struct BSTreeNode_t* node) {
+	while (node->bs_left)
+		node = node->bs_left;
+	return (struct BSTreeNode_t*)node;
+}
+
+struct BSTreeNode_t* bstreeMaxNode(const struct BSTreeNode_t* node) {
+	while (node->bs_right)
+		node = node->bs_right;
+	return (struct BSTreeNode_t*)node;
+}
+
+struct BSTreeNode_t* bstreeSearchKey(struct BSTree_t* tree, const void* key) {
+	struct BSTreeNode_t* node = tree->bs_root;
+	while (node) {
+		int res = tree->keycmp(node->key, key);
+		if (res < 0)
+			node = node->bs_left;
+		else if (res > 0)
+			node = node->bs_right;
+		else
+			break;
+	}
+	return node;
+}
+
+#ifdef	__cplusplus
+}
+#endif
