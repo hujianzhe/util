2bcaa446c8a747bef49df064453f18472f0b92be
hujianzhe <976784480@qq.com>
2019-10-09 15:01:26 +0800

    fixed bug

--- src/component/channel.c
+++ src/component/channel.c
@@ -608,8 +608,10 @@ static void reactorobject_exec_channel(ReactorObject_t* o, long long timestamp_m
 		int inactive_reason;
 		Channel_t* channel = pod_container_of(cur, Channel_t, node);
 		next = cur->next;
-		if (ev_msec < channel->m_event_msec)
+		if (timestamp_msec < channel->m_event_msec) {
+			reactorSetEventTimestamp(channel->io->reactor, channel->m_event_msec);
 			continue;
+		}
 		channel->m_event_msec = 0;
 		inactive_reason = channel_event_handler(channel, timestamp_msec);
 		if (!inactive_reason)
