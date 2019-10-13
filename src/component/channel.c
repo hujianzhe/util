a5bd6b0faeda0d7f11105e17846ec3ba7c6ecc6d
hujianzhe <976784480@qq.com>
2019-10-13 21:44:40 +0800

    ...

--- src/component/channel.c
+++ src/component/channel.c
@@ -60,7 +60,7 @@ static void channel_detach_handler(Channel_t* channel, int reason, long long tim
 	listRemoveNode(&io->channel_list, &channel->node._);
 	channel->detach(channel, reason);
 	if (!io->channel_list.head)
-		reactorCommitCmd(io->reactor, &io->detachcmd);
+		io->m_valid = 0;
 }
 
 static unsigned char* merge_packet(List_t* list, unsigned int* mergelen) {
