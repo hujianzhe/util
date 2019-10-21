d0da6f7b59c935e399bd241dc2fa68caf80f366f
hujianzhe <976784480@qq.com>
2019-10-21 13:44:14 +0800

    cpp lexical_cast

---
+++ inc/cpp/lexical_cast.h
@@ -0,0 +1,58 @@
+//
+// Created by hujianzhe on 19-10-21.
+//
+
+#ifndef UTIL_CPP_LEXICAL_CAST_H
+#define	UTIL_CPP_LEXICAL_CAST_H
+
+#include <sstream>
+#include <typeinfo>
+
+namespace util {
+class bad_lexical_cast : public std::bad_cast {
+public:
+	virtual const char* what(void) const throw() {
+		return "bad lexical cast: source type could not be interpreted as target type";
+	}
+};
+
+template <typename T, typename F>
+T lexical_cast(const F& f) {
+	std::stringstream ss;
+	ss << f;
+	if (!ss)
+		throw bad_lexical_cast();
+	T t;
+	ss >> t;
+	if (!ss)
+		throw bad_lexical_cast();
+	return t;
+}
+
+template <typename T, typename F>
+T lexical_cast_nothrow(const F& f) {
+	std::stringstream ss;
+	ss << f;
+	if (!ss)
+		return T();
+	T t;
+	ss >> t;
+	if (!ss)
+		return T();
+	return t;
+}
+
+template <typename F, typename T>
+bool lexical_cast(const F& f, T& t) {
+	std::stringstream ss;
+	ss << f;
+	if (!ss)
+		return false;
+	ss >> t;
+	if (!ss)
+		return false;
+	return true;
+}
+}
+
+#endif // !UTIL_CPP_LEXICAL_CAST_H
