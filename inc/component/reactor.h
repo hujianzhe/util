6efa85860ee1dbb967cef5637581082b7e35234a
hujianzhe <976784480@qq.com>
2019-10-22 11:23:17 +0800

    ...

--- inc/component/reactor.h
+++ inc/component/reactor.h
@@ -144,6 +144,7 @@ typedef struct ChannelBase_t {
 	};
 	int(*reg)(struct ChannelBase_t* self, long long timestamp_msec);
 	void(*detach)(struct ChannelBase_t* self);
+	int(*on_read)(struct ChannelBase_t* self, unsigned char* buf, unsigned int len, unsigned int off, long long timestamp_msec, const void* from_addr);
 	int(*pre_send)(struct ChannelBase_t* self, ReactorPacket_t* packet, long long timestamp_msec);
 } ChannelBase_t;
 
