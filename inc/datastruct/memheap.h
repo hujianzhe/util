1e1c600f07204fec1c3c8a2ac915a7c0bd3df2ca
hujianzhe <976784480@qq.com>
2019-12-08 14:31:29 +0800

    ...

--- inc/datastruct/memheap.h
+++ inc/datastruct/memheap.h
@@ -9,8 +9,8 @@
 
 struct MemHeap_t;
 
-extern struct MemHeap_t* memheapSetup(void* addr, ptrlen_t len);
-extern void* memheapAlloc(struct MemHeap_t* memheap, ptrlen_t nbytes);
-extern void memheapFree(struct MemHeap_t* memheap, void* addr);
+__declspec_dll struct MemHeap_t* memheapSetup(void* addr, ptrlen_t len);
+__declspec_dll void* memheapAlloc(struct MemHeap_t* memheap, ptrlen_t nbytes);
+__declspec_dll void memheapFree(struct MemHeap_t* memheap, void* addr);
 
 #endif
