33704e113bb3087569c07a2766fef5aad5ac2355
hujianzhe <976784480@qq.com>
2019-12-09 11:47:56 +0800

    ....

--- inc/datastruct/memheap.h
+++ inc/datastruct/memheap.h
@@ -9,9 +9,17 @@
 
 struct MemHeap_t;
 
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
 __declspec_dll struct MemHeap_t* memheapSetup(void* addr, ptrlen_t len, void(*fn_lock_or_unlock)(int));
 __declspec_dll void* memheapAlloc(struct MemHeap_t* memheap, ptrlen_t nbytes);
 __declspec_dll void* memheapAlignAlloc(struct MemHeap_t* memheap, ptrlen_t nbytes, ptrlen_t alignment);
 __declspec_dll void memheapFree(struct MemHeap_t* memheap, void* addr);
+
+#ifdef	__cplusplus
+}
+#endif
 
 #endif
