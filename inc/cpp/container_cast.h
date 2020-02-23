e04f074099b65d29a2b4d5548b4f7859785c81cd
hujianzhe <976784480@qq.com>
2020-02-23 11:52:55 +0800

    cpp container cast

---
+++ inc/cpp/container_cast.h
@@ -0,0 +1,101 @@
+//
+// Created by hujianzhe on 20-2-22.
+//
+
+#ifndef UTIL_CPP_CONTAINER_CAST_H
+#define	UTIL_CPP_CONTAINER_CAST_H
+
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <utility>
+
+namespace util {
+/* vector */
+template <typename T, typename E>
+std::list<E>& vector_shift_list(const std::vector<T>& v, std::list<E>& l) {
+	for (std::vector<T>::const_iterator it = v.begin(); it != v.end(); ++it)
+		l.push_back(*it);
+	return l;
+}
+
+template <typename T, typename E>
+std::set<E>& vector_shift_set(const std::vector<T>& v, std::set<E>& s) {
+	for (std::vector<T>::const_iterator it = v.begin(); it != v.end(); ++it)
+		s.insert(*it);
+	return s;
+}
+
+template <typename K, typename V, typename T, typename E>
+std::map<T, E>& vector_shift_map(const std::vector<std::pair<K, V> >& v, std::map<T, E>& m) {
+	for (std::vector<std::pair<K, V> >::const_iterator it = v.begin(); it != v.end(); ++it)
+		m.insert(*it);
+	return m;
+}
+/* list */
+template <typename T, typename E>
+std::vector<E>& list_shift_vector(const std::list<T>& l, std::vector<E>& v) {
+	for (std::list<T>::const_iterator it = l.begin(); it != l.end(); ++it)
+		v.push_back(*it);
+	return v;
+}
+
+template <typename T, typename E>
+std::set<E>& list_shift_set(const std::list<T>& s, std::set<E>& s) {
+	for (std::list<T>::const_iterator it = l.begin(); it != l.end(); ++it)
+		s.insert(*it);
+	return s;
+}
+
+template <typename K, typename V, typename T, typename E>
+std::map<T, E>& list_shift_map(const std::list<std::pair<K, V> >& l, std::map<T, E>& m) {
+	for (std::list<std::pair<K, V> >::const_iterator it = l.begin(); it != l.end(); ++it)
+		m.insert(*it);
+	return m;
+}
+/* set */
+template <typename T, typename E>
+std::vector<E>& set_shift_vector(const std::set<T>& s, std::vector<E>& v) {
+	for (std::set<T>::const_iterator it = s.begin(); it != s.end(); ++it)
+		v.push_back(*it);
+	return v;
+}
+
+template <typename T, typename E>
+std::list<E>& set_shift_list(const std::set<T>& s, std::list<E>& l) {
+	for (std::set<T>::const_iterator it = s.begin(); it != s.end(); ++it)
+		l.push_back(*it);
+	return l;
+}
+
+template <typename K, typename V, typename T, typename E>
+std::map<T, E>& set_shift_map(const std::set<std::pair<K, V> >& s, std::map<T, E>& m) {
+	for (std::set<std::pair<K, V> >::const_iterator it = s.begin(); it != s.end(); ++it)
+		m.insert(*it);
+	return m;
+}
+/* map */
+template <typename K, typename V, typename T, typename E>
+std::vector<std::pair<T, E> >& map_shift_vector(const std::map<K, V>& m, std::vector<std::pair<T, E> >& v) {
+	for (std::map<K, V>::const_iterator it = m.begin(); it != m.end(); ++it)
+		v.push_back(*it);
+	return v;
+}
+
+template <typename K, typename V, typename T, typename E>
+std::list<std::pair<T, E> >& map_shift_list(const std::map<K, V>& m, std::list<std::pair<T, E> >& l) {
+	for (std::map<K, V>::const_iterator it = m.begin(); it != m.end(); ++it)
+		l.push_back(*it);
+	return l;
+}
+
+template <typename K, typename V, typename T, typename E>
+std::set<std::pair<T, E> >& map_shift_set(const std::map<K, V>& m, std::set<std::pair<T, E> >& s) {
+	for (std::map<K, V>::const_iterator it = m.begin(); it != m.end(); ++it)
+		s.insert(*it);
+	return s;
+}
+}
+
+#endif // !UTIL_CPP_CONTAINER_CAST_H
