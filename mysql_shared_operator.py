bc20c7972e62765bc6765fecfa50477c1dc150e7
hujianzhe <976784480@qq.com>
2020-02-28 01:55:18 +0800

    mysql_shared_operator.py

---
+++ mysql_shared_operator.py
@@ -0,0 +1,195 @@
+#coding=utf-8
+import getopt
+import sys
+import os
+import MySQLdb
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+# 解析命令行
+conn_shared_config_path = "db_connection_config"
+conn_shared_key = None
+
+db_name = None
+db_shared_key = None
+db_shared_cnt = None
+
+table_name = None
+table_shared_key = None
+table_shared_cnt = None
+
+sql_operator = None
+sql_field = None
+sql_where = None
+
+opt_array = ["conn_shared=", "db_name=", "db_shared=", "db_shared_cnt=", "table_name=", "table_shared=", "table_shared_cnt=",
+"operator=", "field=", "where=", "config_path="]
+try:
+	opts, args = getopt.getopt(sys.argv[1:], "", opt_array)
+	for pair in opts:
+		if pair[0] == '--conn_shared':
+			conn_shared_key = int(pair[1])
+		elif pair[0] == '--db_name':
+			db_name = pair[1]
+		elif pair[0] == '--db_shared':
+			db_shared_key = int(pair[1])
+		elif pair[0] == '--db_shared_cnt':
+			db_shared_cnt = int(pair[1])
+		elif pair[0] == '--table_name':
+			table_name = pair[1]
+		elif pair[0] == '--table_shared':
+			table_shared_key = int(pair[1])
+		elif pair[0] == '--table_shared_cnt':
+			table_shared_cnt = int(pair[1])
+		elif pair[0] == '--operator':
+			sql_operator = pair[1]
+		elif pair[0] == '--field':
+			sql_field = pair[1]
+		elif pair[0] == '--where':
+			sql_where = pair[1]
+		elif pair[0] == '--config_path':
+			conn_shared_config_path = pair[1]
+
+	if not conn_shared_key:
+		print '未指定连接分片,--conn_shared'
+		exit(1)
+	elif not db_name:
+		print '未指定db名,--db_name'
+		exit(1)
+	elif not table_name:
+		print '未指定table名,--table_name'
+		exit(1)
+	elif db_shared_key != None and db_shared_cnt == None:
+		print '未指定db总数,--db_shared_cnt'
+		exit(1)
+	elif db_shared_key == None and db_shared_cnt != None:
+		print '未指定db分片,--db_shared'
+		exit(1)
+	elif table_shared_key != None and table_shared_cnt == None:
+		print '未指定table总数,--table_shared_cnt'
+		exit(1)
+	elif table_shared_key == None and table_shared_cnt != None:
+		print '未指定table分片,--table_shared'
+		exit(1)
+	elif sql_operator == None:
+		print '未指定SQL操作,--operator'
+		exit(1)
+
+except getopt.GetoptError:
+	print '解析命令参数错误'
+	exit(1)
+
+print '-------------------------------------------------------'
+
+# 读取连接分片配置
+conn_shared_options = []
+print '连接分片配置路径: %s' % conn_shared_config_path
+f = open(conn_shared_config_path)
+for line in f:
+	conn_shared_options.append(eval(line))
+f.close()
+if len(conn_shared_options) == 0:
+	print '连接配置为空'
+	exit(1)
+else:
+	for conn_option in conn_shared_options:
+		print conn_option
+
+# 组合分片信息
+conn_shared_index = conn_shared_key % len(conn_shared_options)
+conn_option = conn_shared_options[conn_shared_index]
+
+db_shared_index = None
+if db_shared_key != None:
+	db_shared_index = db_shared_key % db_shared_cnt
+	db_name = db_name + '_' + str(db_shared_index)
+
+table_shared_index = None
+if table_shared_key != None:
+	table_shared_index = table_shared_key % table_shared_cnt
+	table_name = table_name + '_' + str(table_shared_index)
+
+# 拼接SQL语句
+sql = ''
+sql_is_select = False
+if sql_operator.lower() == 'select':
+	sql_is_select = True
+	if sql_field == None:
+		print '未指定查询字段,--field'
+		exit(1)
+	if sql_where == None:
+		print '未指定查询条件,--where'
+		exit(1)
+
+	sql = 'select ' + sql_field + ' from ' + table_name + ' where ' + sql_where
+
+elif sql_operator.lower() == 'insert':
+	if sql_field == None:
+		print '未指定插入字段,--field'
+		exit(1)
+
+	sql = 'insert into ' + table_name + ' ' + sql_field
+
+elif sql_operator.lower() == 'update':
+	if sql_field == None:
+		print '未指定更新字段,--field'
+		exit(1)
+	if sql_where == None:
+		print '未指定更新条件,--where'
+		exit(1)
+
+	sql = 'update ' + table_name + ' set ' + sql_field + ' where ' + sql_where
+
+elif sql_operator.lower() == 'delete':
+	if sql_where == None:
+		print '未指定删除条件,--where'
+		exit(1)
+
+	sql = 'delete from' + table_name + ' where ' + sql_where
+
+else:
+	print '不合法的SQL操作 %s' % sql_operator
+	exit(1)
+
+# 打印分片信息
+print '-------------------------------------------------------------'
+print 'db_conn_shared_index = %d' %  conn_shared_index
+if db_shared_index != None:
+	print 'db_shared_index = %d' %  db_shared_index
+if table_shared_index != None:
+	print 'table_shared_index = %d' %  table_shared_index
+print conn_option, db_name, table_name
+print sql
+print '-------------------------------------------------------------'
+print '-------------------------------------------------------------'
+print '-------------------------------------------------------------'
+
+# DB操作
+db_conn_obj = MySQLdb.connect(host=conn_option['host'], port=conn_option['port'] ,user=conn_option['user'], passwd=conn_option['pwd'], db=db_name, charset='utf8')
+db_conn_cursor = db_conn_obj.cursor()
+if not db_conn_cursor:
+	print 'DB连接失败'
+	exit(1)
+
+try:
+	db_conn_cursor.execute(sql)
+	if sql_is_select:
+		result_rows = db_conn_cursor.fetchall()
+		result_cols = db_conn_cursor.description
+		for i in range(len(result_cols)):
+			print result_cols[i][0],'	',	#不换行输出, note: python2 3 语法不兼容
+		print ''
+		print '---------------------------------------------'
+		for i in range(len(result_rows)):
+			for j in range(len(result_rows[i])):
+				print result_rows[i][j],'	',
+			print ''
+	else:
+		db_conn_obj.commit()
+
+except:
+	print 'SQL执行失败'
+	if not sql_is_select:
+		db_conn_obj.rollback()
+
+db_conn_obj.close()
