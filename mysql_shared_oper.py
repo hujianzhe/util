f64195b507159b19ec2e93a1b2966f1665d5f450
hujianzhe <976784480@qq.com>
2020-02-29 21:16:22 +0800

    ...

--- mysql_shared_oper.py
+++ mysql_shared_oper.py
@@ -9,6 +9,7 @@ sys.setdefaultencoding('utf8')
 # 解析命令行
 conn_shared_config_path = "db_connection_config"
 conn_shared_key = None
+conn_shared_cnt = None
 
 db_name = None
 db_shared_key = None
@@ -21,14 +22,19 @@ table_shared_cnt = None
 sql_operator = None
 sql_field = None
 sql_where = None
+sql_is_select = False
 
-opt_array = ["conn_shared=", "db_name=", "db_shared=", "db_shared_cnt=", "table_name=", "table_shared=", "table_shared_cnt=",
-"select=", "update=", "insert=", "delete", "where=", "config_path="]
+is_print_debug_info = False
+
+opt_array = ["conn_shared=", "conn_shared_cnt=", "db_name=", "db_shared=", "db_shared_cnt=", "table_name=", "table_shared=", "table_shared_cnt=",
+"select=", "update=", "insert=", "delete", "where=", "config_path=", "debug"]
 try:
 	opts, args = getopt.getopt(sys.argv[1:], "", opt_array)
 	for pair in opts:
 		if pair[0] == '--conn_shared':
 			conn_shared_key = int(pair[1])
+		elif pair[0] == '--conn_shared_cnt':
+			conn_shared_cnt = int(pair[1])
 		elif pair[0] == '--db_name':
 			db_name = pair[1]
 		elif pair[0] == '--db_shared':
@@ -44,6 +50,7 @@ try:
 		elif pair[0] == '--select':
 			sql_operator = 'select'
 			sql_field = pair[1]
+			sql_is_select = True
 		elif pair[0] == '--update':
 			sql_operator = 'update'
 			sql_field = pair[1]
@@ -56,6 +63,8 @@ try:
 			sql_where = pair[1]
 		elif pair[0] == '--config_path':
 			conn_shared_config_path = pair[1]
+		elif pair[0] == '--debug':
+			is_print_debug_info = True
 
 	if conn_shared_key == None:
 		print '未指定连接分片,--conn_shared'
@@ -90,20 +99,21 @@ print '-------------------------------------------------------'
 
 # 读取连接分片配置
 conn_shared_options = []
-print '连接分片配置路径: %s' % conn_shared_config_path
 f = open(conn_shared_config_path)
 for line in f:
 	conn_shared_options.append(eval(line))
 f.close()
-if len(conn_shared_options) == 0:
-	print '连接配置为空'
+if conn_shared_cnt == None:
+	conn_shared_cnt = len(conn_shared_options)
+elif conn_shared_cnt != len(conn_shared_options):
+	print '配置的分片数和指定的分片数不同'
 	exit(1)
-else:
-	for conn_option in conn_shared_options:
-		print conn_option
+if conn_shared_cnt == 0:
+	print '连接配置数目为空'
+	exit(1)
 
 # 组合分片信息
-conn_shared_index = conn_shared_key % len(conn_shared_options)
+conn_shared_index = conn_shared_key % conn_shared_cnt
 conn_option = conn_shared_options[conn_shared_index]
 
 db_shared_index = None
@@ -118,9 +128,7 @@ if table_shared_key != None:
 
 # 拼接SQL语句
 sql = ''
-sql_is_select = False
 if sql_operator.lower() == 'select':
-	sql_is_select = True
 	if sql_field == None:
 		print '未指定查询字段,--select'
 		exit(1)
@@ -159,17 +167,19 @@ else:
 	exit(1)
 
 # 打印分片信息
-print '-------------------------------------------------------------'
-print 'db_conn_shared_index = %d' %  conn_shared_index
-if db_shared_index != None:
-	print 'db_shared_index = %d' %  db_shared_index
-if table_shared_index != None:
-	print 'table_shared_index = %d' %  table_shared_index
-print conn_option, db_name, table_name
-print sql
-print '-------------------------------------------------------------'
-print '-------------------------------------------------------------'
-print '-------------------------------------------------------------'
+if is_print_debug_info:
+	print '-------------------------------------------------------------'
+	print '连接分片配置路径: %s' % conn_shared_config_path
+	for conn_option_item in conn_shared_options:
+		print conn_option_item
+	print 'db_conn_shared_index = %d' %  conn_shared_index
+	if db_shared_index != None:
+		print 'db_shared_index = %d' %  db_shared_index
+	if table_shared_index != None:
+		print 'table_shared_index = %d' %  table_shared_index
+	print conn_option, db_name, table_name
+	print sql
+	print '-------------------------------------------------------------'
 
 # DB操作
 db_conn_obj = MySQLdb.connect(host=conn_option['host'], port=conn_option['port'] ,user=conn_option['user'], passwd=conn_option['pwd'], db=db_name, charset='utf8')
@@ -194,8 +204,8 @@ try:
 	else:
 		db_conn_obj.commit()
 
-except:
-	print 'SQL执行失败'
+except Exception as e:
+	print 'SQL执行失败', e
 	if not sql_is_select:
 		db_conn_obj.rollback()
 
