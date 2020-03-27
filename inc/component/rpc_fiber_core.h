de3c1daf72f3c97c31a0b6b11ce9a3e3302904f5
hujianzhe <976784480@qq.com>
2020-03-27 17:36:01 +0800

    ...

--- inc/component/rpc_fiber_core.h
+++ inc/component/rpc_fiber_core.h
@@ -13,31 +13,39 @@ typedef struct RpcItem_t {
 	int id;
 	long long timestamp_msec;
 	long long timeout_msec;
+	void* req_ctx;
 	void* ret_msg;
 } RpcItem_t;
 
 typedef struct RpcFiberCore_t {
 	Fiber_t* fiber;
 	Fiber_t* sche_fiber;
-	RBTree_t reg_rpc_tree;
+	RBTree_t reg_tree;
 	void* new_msg;
 	void* disconnect_cmd;
 	void(*msg_handler)(struct RpcFiberCore_t*, void*);
 } RpcFiberCore_t;
 
+typedef struct RpcAsyncCore_t {
+	RBTree_t reg_tree;
+} RpcAsyncCore_t;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+RpcAsyncCore_t* rpcAsyncCoreInit(RpcAsyncCore_t* rpc);
+void rpcAsyncCoreDestroy(RpcAsyncCore_t* rpc);
+RpcItem_t* rpcAsyncCoreRegItem(RpcAsyncCore_t* rpc, int rpcid, long long timeout_msec, void* req_ctx);
+RpcItem_t* rpcAsyncCoreExistItem(RpcAsyncCore_t* rpc, int rpcid);
+void rpcAsyncCoreFreeItem(RpcAsyncCore_t* rpc, RpcItem_t* item);
+
 RpcFiberCore_t* rpcFiberCoreInit(RpcFiberCore_t* rpc, Fiber_t* sche_fiber, size_t stack_size);
 void rpcFiberCoreDestroy(RpcFiberCore_t* rpc);
-
 RpcItem_t* rpcFiberCoreExistItem(RpcFiberCore_t* rpc, int rpcid);
 void rpcFiberCoreFreeItem(RpcFiberCore_t* rpc, RpcItem_t* item);
-
 RpcItem_t* rpcFiberCoreReturnWait(RpcFiberCore_t* rpc, int rpcid, long long timeout_msec);
 int rpcFiberCoreReturnSwitch(RpcFiberCore_t* rpc, int rpcid, void* ret_msg);
-
 void rpcFiberCoreMessageHandleSwitch(RpcFiberCore_t* rpc, void* new_msg);
 void rpcFiberCoreDisconnectHandleSwitch(RpcFiberCore_t* rpc, void* disconnect_cmd);
 
--- src/component/rpc_fiber_core.c
+++ src/component/rpc_fiber_core.c
@@ -10,6 +10,62 @@
 extern "C" {
 #endif
 
+static RpcItem_t* rpc_reg_item(RBTree_t* rpc_reg_tree, int rpcid, long long timeout_msec) {
+	RpcItem_t* item = (RpcItem_t*)malloc(sizeof(RpcItem_t));
+	if (item) {
+		RBTreeNode_t* exist_node;
+		item->m_treenode.key = (const void*)(size_t)rpcid;
+		exist_node = rbtreeInsertNode(rpc_reg_tree, &item->m_treenode);
+		if (exist_node != &item->m_treenode) {
+			free(item);
+			return NULL;
+		}
+		item->id = rpcid;
+		item->timestamp_msec = gmtimeMillisecond();
+		item->timeout_msec = timeout_msec;
+		item->req_ctx = NULL;
+		item->ret_msg = NULL;
+	}
+	return item;
+}
+
+static int __keycmp(const void* node_key, const void* key) {
+	return ((int)(size_t)node_key) - (int)((size_t)key);
+}
+
+/*****************************************************************************************/
+
+RpcAsyncCore_t* rpcAsyncCoreInit(RpcAsyncCore_t* rpc) {
+	rbtreeInit(&rpc->reg_tree, __keycmp);
+	return rpc;
+}
+
+void rpcAsyncCoreDestroy(RpcAsyncCore_t* rpc) {
+	// TODO
+}
+
+RpcItem_t* rpcAsyncCoreRegItem(RpcAsyncCore_t* rpc, int rpcid, long long timeout_msec, void* req_ctx) {
+	RpcItem_t* rpc_item = rpc_reg_item(&rpc->reg_tree, rpcid, timeout_msec);
+	if (rpc_item) {
+		rpc_item->req_ctx = req_ctx;
+	}
+	return rpc_item;
+}
+
+RpcItem_t* rpcAsyncCoreExistItem(RpcAsyncCore_t* rpc, int rpcid) {
+	RBTreeNode_t* node = rbtreeSearchKey(&rpc->reg_tree, (const void*)(size_t)rpcid);
+	return node ? pod_container_of(node, RpcItem_t, m_treenode) : NULL;
+}
+
+void rpcAsyncCoreFreeItem(RpcAsyncCore_t* rpc, RpcItem_t* item) {
+	if (item) {
+		rbtreeRemoveNode(&rpc->reg_tree, &item->m_treenode);
+		free(item);
+	}
+}
+
+/*****************************************************************************************/
+
 static void RpcFiberProcEntry(Fiber_t* fiber) {
 	RpcFiberCore_t* rpc = (RpcFiberCore_t*)fiber->arg;
 	while (1) {
@@ -28,28 +84,6 @@ static void RpcFiberProcEntry(Fiber_t* fiber) {
 	fiberSwitch(fiber, rpc->sche_fiber);
 }
 
-static RpcItem_t* rpc_reg_item(RpcFiberCore_t* rpc, int rpcid, long long timeout_msec) {
-	RpcItem_t* item = (RpcItem_t*)malloc(sizeof(RpcItem_t));
-	if (item) {
-		RBTreeNode_t* exist_node;
-		item->m_treenode.key = (const void*)(size_t)rpcid;
-		exist_node = rbtreeInsertNode(&rpc->reg_rpc_tree, &item->m_treenode);
-		if (exist_node != &item->m_treenode) {
-			free(item);
-			return NULL;
-		}
-		item->id = rpcid;
-		item->timestamp_msec = gmtimeMillisecond();
-		item->timeout_msec = timeout_msec;
-		item->ret_msg = NULL;
-	}
-	return item;
-}
-
-static int __keycmp(const void* node_key, const void* key) {
-	return ((int)(size_t)node_key) - (int)((size_t)key);
-}
-
 RpcFiberCore_t* rpcFiberCoreInit(RpcFiberCore_t* rpc, Fiber_t* sche_fiber, size_t stack_size) {
 	rpc->fiber = fiberCreate(sche_fiber, stack_size, RpcFiberProcEntry);
 	if (!rpc->fiber) {
@@ -61,7 +95,7 @@ RpcFiberCore_t* rpcFiberCoreInit(RpcFiberCore_t* rpc, Fiber_t* sche_fiber, size_
 	rpc->new_msg = NULL;
 	rpc->disconnect_cmd = NULL;
 	rpc->msg_handler = NULL;
-	rbtreeInit(&rpc->reg_rpc_tree, __keycmp);
+	rbtreeInit(&rpc->reg_tree, __keycmp);
 	return rpc;
 }
 
@@ -70,19 +104,19 @@ void rpcFiberCoreDestroy(RpcFiberCore_t* rpc) {
 }
 
 RpcItem_t* rpcFiberCoreExistItem(RpcFiberCore_t* rpc, int rpcid) {
-	RBTreeNode_t* node = rbtreeSearchKey(&rpc->reg_rpc_tree, (const void*)(size_t)rpcid);
+	RBTreeNode_t* node = rbtreeSearchKey(&rpc->reg_tree, (const void*)(size_t)rpcid);
 	return node ? pod_container_of(node, RpcItem_t, m_treenode) : NULL;
 }
 
 void rpcFiberCoreFreeItem(RpcFiberCore_t* rpc, RpcItem_t* item) {
 	if (item) {
-		rbtreeRemoveNode(&rpc->reg_rpc_tree, &item->m_treenode);
+		rbtreeRemoveNode(&rpc->reg_tree, &item->m_treenode);
 		free(item);
 	}
 }
 
 RpcItem_t* rpcFiberCoreReturnWait(RpcFiberCore_t* rpc, int rpcid, long long timeout_msec) {
-	RpcItem_t* rpc_item = rpc_reg_item(rpc, rpcid, timeout_msec);
+	RpcItem_t* rpc_item = rpc_reg_item(&rpc->reg_tree, rpcid, timeout_msec);
 	if (rpc_item) {
 		fiberSwitch(rpc->fiber, rpc->sche_fiber);
 		while (rpc->new_msg) {
