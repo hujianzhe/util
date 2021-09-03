dd74a390a94ed130d1f345045c2fa2d68755d1b0
hujianzhe <976784480@qq.com>
2021-09-04 02:01:42 +0800

    enhance lib_compile script

--- lib_compile.sh
+++ lib_compile.sh
@@ -2,26 +2,35 @@
 OBJECT_FILE=`find . -name "*.o"`
 SOURCE_C_FILE=`find . -name "*.c"`
 SOURCE_CPP_FILE=`find . -name "*.cpp"`
-if [ $SOURCE_CPP_FILE ];then
-	COMPILER="g++"
-else
-	COMPILER="gcc"
-fi
-SOURCE_FILE=$SOURCE_C_FILE" $SOURCE_CPP_FILE"
 MACRO="-D_REENTRANT"
 COMPILE_OPTION="-Wno-deprecated -Wno-parentheses"
-TARGET="libutil_static.a"
+
+COMPILER="gcc"
+if [ $COMPILER == "gcc" ];then
+	SOURCE_CPP_FILE=""
+fi
+BUILD="debug"
+if [ $BUILD == "debug" ];then
+	MACRO="-D_DEBUG $MACRO"
+	COMPILE_OPTION="-g $COMPILE_OPTION"
+	TARGET="libutil_static_debug.a"
+else
+	MACRO="-DNDEBUG $MACRO"
+	COMPILE_OPTION="-O1 $COMPILE_OPTION"
+	TARGET="libutil_static_release.a"
+fi
 
 rm $OBJECT_FILE 2>/dev/null
 rm $TARGET 2>/dev/null
-find ./ -type f -exec touch {} \;
-echo $COMPILER is compiling......
-$COMPILER -c $MACRO $COMPILE_OPTION $SOURCE_FILE
+#find ./ -type f -exec touch {} \;
+echo "$COMPILER -c $MACRO $COMPILE_OPTION" $SOURCE_C_FILE $SOURCE_CPP_FILE 
+$COMPILER -c $MACRO $COMPILE_OPTION $SOURCE_C_FILE $SOURCE_CPP_FILE
 if [ "$?" != 0 ];then
 	OBJECT_FILE=`find . -name "*.o"`
 	rm $OBJECT_FILE 2>/dev/null
 	exit
 fi
 OBJECT_FILE=`find . -name "*.o"`
+echo "ar rcs $TARGET" $OBJECT_FILE
 ar rcs $TARGET $OBJECT_FILE
 rm $OBJECT_FILE 2>/dev/null
