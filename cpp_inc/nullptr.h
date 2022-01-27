7849fb89a7f066d33b6cf702c6cb9aebde2bc898
hujianzhe <976784480@qq.com>
2022-01-28 03:00:28 +0800

    revert code

--- cpp_inc/nullptr.h
+++ cpp_inc/nullptr.h
@@ -8,7 +8,7 @@
 #include "cpp_compiler_define.h"
 #include <cstddef>
 
-#if	__CPP_VERSION < 2011 && !defined(__clang__)
+#if	__CPP_VERSION < 2011
 namespace std {
 typedef struct nullptr_t {
 	nullptr_t(void) : __p((void*)0) {}
