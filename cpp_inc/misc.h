ce81a8d0f62fd802b88958944c80107fe5153670
hujianzhe <976784480@qq.com>
2022-03-17 10:17:52 +0800

    cstruct_wrap

--- cpp_inc/misc.h
+++ cpp_inc/misc.h
@@ -2,10 +2,32 @@
 // Created by hujianzhe on 22-3-16
 //
 
+#include <string.h>
+
 namespace util {
-// if T don't have virtual table, T* don't cast to cstruct_wrap<T>*
 template <typename T>
 struct cstruct_wrap : public T {
 	virtual ~cstruct_wrap() {}
+
+	void bzero() {
+		T* base = this;
+		memset(base, 0, sizeof(T));
+	}
+
+	void copy(const void* p, size_t n) {
+		T* base = this;
+		if (base == p) {
+			return;
+		}
+		memcpy(base, p, n > sizeof(T) ? sizeof(T) : n);
+	}
+	void copy(const T* t) {
+		T* base = this;
+		if (base == t) {
+			return;
+		}
+		memcpy(base, t, sizeof(T));
+	}
+	void copy(const T& t) { copy(&t); }
 };
 }
