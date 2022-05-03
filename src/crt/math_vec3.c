d0cee9813473c4bb38c18c41afe0841bb6e49b7e
hujianzhe <976784480@qq.com>
2022-05-03 10:10:06 +0800

    optimized code

--- src/crt/math_vec3.c
+++ src/crt/math_vec3.c
@@ -89,8 +89,9 @@ float mathVec3Len(const float v[3]) {
 }
 
 float mathVec3Normalized(float r[3], const float v[3]) {
-	float len = mathVec3Len(v);
-	if (len > CCT_EPSILON) {
+	float len_sq = v[0] * v[0] + v[1] * v[1] + v[2] * v[2];
+	if (len_sq > CCT_EPSILON) {
+		float len = sqrtf(len_sq);
 		float inv_len = 1.0f / len;
 		r[0] = v[0] * inv_len;
 		r[1] = v[1] * inv_len;
