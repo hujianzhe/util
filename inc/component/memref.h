3e91d85d0c32526abc8ece1eb6b61a7604ac1ab3
hujianzhe <976784480@qq.com>
2022-12-13 17:51:34 +0800

    memref

--- inc/component/memref.h
+++ inc/component/memref.h
@@ -13,12 +13,12 @@ struct MemRef_t;
 extern "C" {
 #endif
 
-__declspec_dll struct MemRef_t* memrefCreate(void* p);
+__declspec_dll struct MemRef_t* memrefCreate(void* p, void(*fn_free)(void*));
+__declspec_dll void* memrefGetPtr(struct MemRef_t* ref);
+__declspec_dll struct MemRef_t* memrefIncr(struct MemRef_t* ref);
+__declspec_dll void memrefDecr(struct MemRef_t** p_ref);
 
-__declspec_dll struct MemRef_t* memrefIncrStrong(struct MemRef_t* ref);
-__declspec_dll void* memrefDecrStrong(struct MemRef_t** p_ref);
-
-__declspec_dll void* memrefLock(struct MemRef_t* ref);
+__declspec_dll struct MemRef_t* memrefLockWeak(struct MemRef_t* ref);
 __declspec_dll struct MemRef_t* memrefIncrWeak(struct MemRef_t* ref);
 __declspec_dll void memrefDecrWeak(struct MemRef_t** p_ref);
 
--- src/component/memref.c
+++ src/component/memref.c
@@ -6,23 +6,25 @@
 #include "../../inc/sysapi/atomic.h"
 #include <stdlib.h>
 
-struct MemRef_t {
+typedef struct MemRef_t {
 	void* p;
+	void(*fn_free)(void*);
 	Atom32_t sp_cnt;
 	Atom32_t wp_cnt;
-};
+} MemRef_t;
 
 #ifdef	__cplusplus
 extern "C" {
 #endif
 
-struct MemRef_t* memrefCreate(void* p) {
+MemRef_t* memrefCreate(void* p, void(*fn_free)(void*)) {
 	if (p) {
-		struct MemRef_t* ref = (struct MemRef_t*)malloc(sizeof(struct MemRef_t));
+		MemRef_t* ref = (MemRef_t*)malloc(sizeof(MemRef_t));
 		if (!ref) {
 			return NULL;
 		}
 		ref->p = p;
+		ref->fn_free = fn_free;
 		ref->sp_cnt = 1;
 		ref->wp_cnt = 1;
 		return ref;
@@ -30,33 +32,37 @@ struct MemRef_t* memrefCreate(void* p) {
 	return NULL;
 }
 
-struct MemRef_t* memrefIncrStrong(struct MemRef_t* ref) {
+void* memrefGetPtr(MemRef_t* ref) { return ref ? ref->p : NULL; }
+
+MemRef_t* memrefIncr(MemRef_t* ref) {
 	if (ref) {
 		_xadd32(&ref->sp_cnt, 1);
 	}
 	return ref;
 }
 
-void* memrefDecrStrong(struct MemRef_t** p_ref) {
-	struct MemRef_t* ref = *p_ref;
-	if (ref) {
-		void* p;
-		Atom32_t sp_cnt = _xadd32(&ref->sp_cnt, -1);
-		if (sp_cnt > 1) {
-			return NULL;
-		}
-		p = ref->p;
-		ref->p = NULL;
-		if (_xadd32(&ref->wp_cnt, -1) <= 1) {
-			free(ref);
-			*p_ref = NULL;
-		}
-		return p;
+void memrefDecr(MemRef_t** p_ref) {
+	Atom32_t sp_cnt;
+	MemRef_t* ref = *p_ref;
+	if (!ref) {
+		return;
 	}
-	return NULL;
+	*p_ref = NULL;
+
+	sp_cnt = _xadd32(&ref->sp_cnt, -1);
+	if (sp_cnt > 1) {
+		return;
+	}
+	if (_xadd32(&ref->wp_cnt, -1) > 1) {
+		return;
+	}
+	if (ref->fn_free) {
+		ref->fn_free(ref->p);
+	}
+	free(ref);
 }
 
-void* memrefLock(struct MemRef_t* ref) {
+MemRef_t* memrefLockWeak(MemRef_t* ref) {
 	if (!ref || !ref->p) {
 		return NULL;
 	}
@@ -66,27 +72,29 @@ void* memrefLock(struct MemRef_t* ref) {
 			return NULL;
 		}
 		if (_cmpxchg32(&ref->sp_cnt, tmp + 1, tmp) == tmp) {
-			return ref->p;
+			return ref;
 		}
 	}
 }
 
-struct MemRef_t* memrefIncrWeak(struct MemRef_t* ref) {
+MemRef_t* memrefIncrWeak(MemRef_t* ref) {
 	if (ref) {
 		_xadd32(&ref->wp_cnt, 1);
 	}
 	return ref;
 }
 
-void memrefDecrWeak(struct MemRef_t** p_ref) {
-	struct MemRef_t* ref = *p_ref;
+void memrefDecrWeak(MemRef_t** p_ref) {
+	MemRef_t* ref = *p_ref;
 	if (!ref) {
 		return;
 	}
-	if (_xadd32(&ref->wp_cnt, -1) <= 1) {
-		free(ref);
-		*p_ref = NULL;
+	*p_ref = NULL;
+
+	if (_xadd32(&ref->wp_cnt, -1) > 1) {
+		return;
 	}
+	free(ref);
 }
 
 #ifdef	__cplusplus
