10a8a00aadc4bef2fde305091ec355cbc7c457b4
hujianzhe <976784480@qq.com>
2023-04-25 15:30:16 +0800

    remove vkeycode

--- inc/sysapi/terminal.h
+++ inc/sysapi/terminal.h
@@ -12,20 +12,6 @@
 #else
 	#include <termios.h>
 #endif
-#include "vkeycode.h"
-
-typedef struct DevKeyEvent_t {
-	unsigned char keydown;
-	unsigned int vkeycode;
-#if defined(_WIN32) || defined(_WIN64)
-	unsigned int charcode;
-#else
-	union {
-		unsigned char __charbuf[8];
-		unsigned int charcode;
-	};
-#endif
-} DevKeyEvent_t;
 
 #ifdef	__cplusplus
 extern "C" {
@@ -44,9 +30,6 @@ __declspec_dll BOOL terminalGetPageSize(FD_t fd, int* x_col, int* y_row);
 __declspec_dll BOOL terminalSetCursorPos(FD_t fd, int x_col, int y_row);
 __declspec_dll BOOL terminalShowCursor(FD_t fd, BOOL bval);
 __declspec_dll BOOL terminalClrscr(FD_t fd);
-__declspec_dll BOOL terminalKeyDevScan(void);
-__declspec_dll BOOL terminalReadKey(FD_t fd, DevKeyEvent_t* e);
-__declspec_dll BOOL terminalReadKeyNonBlock(FD_t fd, DevKeyEvent_t* e);
 
 #ifdef	__cplusplus
 }
