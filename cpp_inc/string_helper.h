fa9d0d3525156f6cc1a0fd437c020aff32802036
hujianzhe <976784480@qq.com>
2024-01-23 00:05:24 +0800

    string help class

--- cpp_inc/string_helper.h
+++ cpp_inc/string_helper.h
@@ -13,76 +13,76 @@
 #include <vector>
 
 namespace util {
-template <typename StdContainer>
-void string_split(const char* str, int delim, StdContainer<std::string>& v) {
-	const char* p;
-	for (p = str; *str; ++str) {
-		if (*p == delim) {
-			v.push_back(std::string(p, str - p));
-			p = str + 1;
-		}
-	}
-	if (str != p) {
-		v.push_back(std::string(p, str - p));
-	}
-}
-
-template <typename StdContainer>
-void string_split(const char* str, const char* delim, StdContainer<std::string>& v) {
-	const char* p, *dp;
-	for (p = str; *str; ++str) {
-		for (dp = delim; *dp; ++dp) {
-			if (*dp == *str) {
+class string {
+public:
+	void split(const char* str, int delim, std::vector<std::string>& v) {
+		const char* p;
+		for (p = str; *str; ++str) {
+			if (*p == delim) {
 				v.push_back(std::string(p, str - p));
 				p = str + 1;
-				break;
 			}
 		}
+		if (str != p) {
+			v.push_back(std::string(p, str - p));
+		}
 	}
-	if (str != p) {
-		v.push_back(std::string(p, str - p));
-	}
-}
 
-template <typename StdContainer>
-void string_splits(const char* str, const char* delim, StdContainer<std::string>& v) {
-	const char* p;
-	size_t delim_len = strlen(delim);
-	while ((p = strstr(str, delim))) {
-		v.push_back(std::string(str, p - str));
-		str = p + delim_len;
+	void split(const char* str, const char* delim, std::vector<std::string>& v) {
+		const char* p, *dp;
+		for (p = str; *str; ++str) {
+			for (dp = delim; *dp; ++dp) {
+				if (*dp == *str) {
+					v.push_back(std::string(p, str - p));
+					p = str + 1;
+					break;
+				}
+			}
+		}
+		if (str != p) {
+			v.push_back(std::string(p, str - p));
+		}
 	}
-	if (*str) {
-		v.push_back(str);
-	}
-}
 
-std::string string_format(const char* format, ...) {
-	char test_buf;
-	char* buf;
-	int len;
-	va_list varg;
-	va_start(varg, format);
-	len = vsnprintf(&test_buf, 0, format, varg);
-	va_end(varg);
-	if (len <= 0) {
-		return std::string();
+	void splits(const char* str, const char* delim, std::vector<std::string>& v) {
+		const char* p;
+		size_t delim_len = strlen(delim);
+		while ((p = strstr(str, delim))) {
+			v.push_back(std::string(str, p - str));
+			str = p + delim_len;
+		}
+		if (*str) {
+			v.push_back(str);
+		}
 	}
-	buf = (char*)malloc(len + 1);
-	if (!buf) {
-		return std::string();
-	}
-	va_start(varg, format);
-	len = vsnprintf(buf, len + 1, format, varg);
-	va_end(varg);
-	if (len <= 0) {
+
+	std::string format(const char* format, ...) {
+		char test_buf;
+		char* buf;
+		int len;
+		va_list varg;
+		va_start(varg, format);
+		len = vsnprintf(&test_buf, 0, format, varg);
+		va_end(varg);
+		if (len <= 0) {
+			return std::string();
+		}
+		buf = (char*)malloc(len + 1);
+		if (!buf) {
+			return std::string();
+		}
+		va_start(varg, format);
+		len = vsnprintf(buf, len + 1, format, varg);
+		va_end(varg);
+		if (len <= 0) {
+			free(buf);
+			return std::string();
+		}
+		std::string s(buf, len);
 		free(buf);
-		return std::string();
+		return s;
 	}
-	std::string s(buf, len);
-	free(buf);
-	return s;
-}
+};
 }
 
 #endif // !UTIL_CPP_STRING_HELPER_H
