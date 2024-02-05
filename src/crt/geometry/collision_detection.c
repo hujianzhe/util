9b91740fae834d730b3b76b6f492d548bed8337c
hujianzhe <976784480@qq.com>
2024-02-05 21:36:00 +0800

    remove include header

--- src/crt/geometry/collision_detection.c
+++ src/crt/geometry/collision_detection.c
@@ -2,7 +2,6 @@
 // Created by hujianzhe
 //
 
-#include "../../../inc/crt/math.h"
 #include "../../../inc/crt/math_vec3.h"
 #include "../../../inc/crt/geometry/line_segment.h"
 #include "../../../inc/crt/geometry/plane.h"
