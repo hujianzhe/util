f5a3e56e37eff5f2f1b7be93416693bbdbbc325a
zzz <zzz@taomee-ex.com>
2024-05-10 14:07:51 +0800

    module

--- inc/sysapi/module.h
+++ inc/sysapi/module.h
@@ -14,20 +14,20 @@
 #endif
 
 #if defined(_WIN32) || defined(_WIN64)
-	#define	moduleLoad(path)							((void*)LoadLibraryA(path))
-	#define	moduleSymbolAddress(module, symbol_name)	((void*)GetProcAddress((HMODULE)module, symbol_name))
-	#define	moduleUnload(module)						(module ? FreeLibrary((HMODULE)module) : TRUE)
+	#define	moduleLoad(path)						((void*)LoadLibraryA(path))
+	#define	moduleSymbolAddress(md, symbol_name)	((void*)GetProcAddress((HMODULE)md, symbol_name))
+	#define	moduleUnload(md)						(md ? FreeLibrary((HMODULE)md) : TRUE)
 #else
-	#define	moduleLoad(path)							dlopen(path, RTLD_NOW)
-	#define	moduleSymbolAddress(module, symbol_name)	dlsym(module, symbol_name)
-	#define	moduleUnload(module)						(module ? (dlclose(module) == 0) : 1)
+	#define	moduleLoad(path)						dlopen(path, RTLD_NOW)
+	#define	moduleSymbolAddress(md, symbol_name)	dlsym(md, symbol_name)
+	#define	moduleUnload(md)						(md ? (dlclose(md) == 0) : 1)
 #endif
 
 #ifdef	__cplusplus
 extern "C" {
 #endif
 
-__declspec_dll void* moduleGetSelf(void);
+__declspec_dll void* moduleGetAddress(const void* symbol_addr);
 
 #ifdef	__cplusplus
 }
--- src/sysapi/module.c
+++ src/sysapi/module.c
@@ -8,17 +8,16 @@
 extern "C" {
 #endif
 
+void* moduleGetAddress(const void* symbol_addr) {
 #if defined(_WIN32) || defined(_WIN64)
-#else
-static void empty_dladdr() {}
-#endif
-
-void* moduleGetSelf(void) {
-#if defined(_WIN32) || defined(_WIN64)
-	return (void*)GetModuleHandle(NULL);
+	HMODULE m;
+	if (GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS, (PTCHAR)symbol_addr, &m)) {
+		return (void*)m;
+	}
+	return NULL;
 #else
 	Dl_info dl_info;
-	if (dladdr((void*)empty_dladdr, &dl_info)) {
+	if (dladdr((void*)symbol_addr, &dl_info)) {
 		return dlopen(dl_info.dli_fname, RTLD_NOW);
 	}
 	return NULL;
