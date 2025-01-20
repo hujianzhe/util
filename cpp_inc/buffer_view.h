b5d461a6f14a6fbbc89cde52d7e079d501c3e955
hujianzhe <976784480@qq.com>
2025-01-20 15:56:56 +0800

    buffer_view

--- cpp_inc/buffer_view.h
+++ cpp_inc/buffer_view.h
@@ -5,6 +5,10 @@
 #ifndef	UTIL_CPP_BUFFER_VIEW_H
 #define	UTIL_CPP_BUFFER_VIEW_H
 
+#include <string.h>
+#include <string>
+#include <vector>
+
 namespace util {
 struct buffer_view {
 	buffer_view() : m_ptr(NULL), m_length(0) {}
@@ -40,6 +44,32 @@ struct buffer_view {
 		return off + len;
 	}
 
+	size_t read(size_t off, std::string* v, size_t len) const {
+		if (!valid(off, len)) {
+			return -1;
+		}
+		v->assign(m_ptr + off, len);
+		return off + len;
+	}
+
+	size_t read(size_t off, std::vector<char>* v, size_t len) const {
+		if (!valid(off, len)) {
+			return -1;
+		}
+		v->resize(len);
+		memcpy(v->data(), m_ptr + off, len);
+		return off + len;
+	}
+
+	size_t read(size_t off, std::vector<unsigned char>* v, size_t len) const {
+		if (!valid(off, len)) {
+			return -1;
+		}
+		v->resize(len);
+		memcpy(v->data(), m_ptr + off, len);
+		return off + len;
+	}
+
 	template <typename T>
 	size_t write(size_t off, const T& v) const {
 		if (!valid(off, sizeof(T))) {
@@ -55,6 +85,18 @@ struct buffer_view {
 		}
 		memcpy(m_ptr + off, p, len);
 		return off + len;
+	}
+
+	size_t write(size_t off, const std::string& v) const {
+		return write(off, v.data(), v.size());
+	}
+
+	size_t write(size_t off, const std::vector<char>& v) const {
+		return write(off, v.data(), v.size());
+	}
+
+	size_t write(size_t off, const std::vector<unsigned char>& v) const {
+		return write(off, v.data(), v.size());
 	}
 
 private:
